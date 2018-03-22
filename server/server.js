43888ffe59512ed11c00172e977eaf29609e534c
“Mahran <Mahran_msri@yahoo.com>
2018-03-22 13:01:33 +0100

    add connect and disconnect socket.io methods

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const path = require("path");
+const http=require('http');
 const publicPath = path.join(__dirname , "../public");
 const express=require('express');
 const app=express();
+const socketIO=require('socket.io')
 const port=process.env.PORT||3000;
+var server=http.createServer(app);
+var io=socketIO(server)
 
 app.use(express.static(publicPath))
+io.on('connection',(socket)=>{
+    console.log('New user connected')
+    socket.on('disconnect',()=>{
+        console.log('client disconnected')
+    })
+})
 
 
-app.listen(port,()=>console.log(`Server started on port ${port}`))
+server.listen(port,()=>{
+    console.log(`Server started on port ${port}`)})
